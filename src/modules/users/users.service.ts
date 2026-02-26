d360a392fb87f8460adf1f7ed7ddf8e228ad42eb
Leandro Frederico <leandro_frederico@hotmail.com>
2026-02-26 09:14:45 -0300

    feat: create user with validation

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@nestjs/common'
-import { PrismaService } from '@/prisma/prisma.service'
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   findAll() {
-    return this.prisma.user.findMany()
+    return this.prisma.user.findMany();
+  }
+
+  create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data,
+    });
   }
 }
