8b8e7f599e682b8eabaafd8c0563ee0278f62750
Leandro Frederico <leandro_frederico@hotmail.com>
2026-02-26 07:52:03 -0300

    feat: integrate prisma 7 with postgres adapter

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getUsers() {
+    return this.prisma.user.findMany()
   }
 }
