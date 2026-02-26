0e716d79fc8e9cc24e9d1a59e8c2611e7f742408
Leandro Frederico <leandro_frederico@hotmail.com>
2026-02-26 07:59:14 -0300

    feat: add users module with prisma integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './modules/app.controller';
 import { AppService } from './modules/app.service';
 import { PrismaModule } from './prisma/prisma.module'
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private prisma: PrismaService) { }
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  async getUsers() {
-    return this.prisma.user.findMany()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
