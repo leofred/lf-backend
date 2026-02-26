72e78ede283c168680c7e6fe148a28850386d4e4
Leandro Frederico <leandro_frederico@hotmail.com>
2026-02-26 09:20:51 -0300

    chore: create auth module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './modules/app.controller';
 import { AppService } from './modules/app.service';
 import { PrismaModule } from './prisma/prisma.module'
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
