2f512f1c02d83562f4250a9f2135d050c599964f
Leandro Frederico <leandro_frederico@hotmail.com>
2026-02-26 09:17:52 -0300

    feat: hash user password with bcrypt

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   findAll() {
     return this.prisma.user.findMany();
   }
 
-  create(data: CreateUserDto) {
+  async create(data: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    
     return this.prisma.user.create({
-      data,
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
     });
   }
 }
